b9b85905e1c56641e62c88a3e2331e783d339617
Leonardo Figueiredo <leommko@gmail.com>
2021-04-22 19:47:37 -0300

    feat: create Constrant class

---
+++ app/src/main/java/com/example/foody/Constrants.kt
@@ -0,0 +1,8 @@
+package com.example.foody
+
+class Constrants {
+    companion object {
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "30cf02508d65420fb123e1733020d9e6"
+    }
+}
